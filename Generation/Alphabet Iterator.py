4eab9d5d7fae63db6a53b1012d2b03b6d1f2d806
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-12-01 11:47:30 +0000

    Update

--- Generation/Alphabet Iterator.py
+++ Generation/Alphabet Iterator.py
@@ -4,20 +4,18 @@ import time
 if not os.path.exists('data'):
         os.makedirs('data')
 
-global nextNum
-global array
-
-nextNum = 96
-array = ['' for i in range(135)]
-
 def iterate_strings(length):
     count = 1
     prefix = ""
     totalCount = 0
     prevLen = 1
+    nextNum = 96
+    start = time.perf_counter()
     
     for i in range(length):
         totalCount += pow(26, i+1)
+
+    array = ['' for i in range(totalCount)]
     
     for i in range(0, totalCount):
         nextNum += 1 
@@ -29,17 +27,28 @@ def iterate_strings(length):
         array[i] = prefix + chr(nextNum)
 
         if len(array[i]) > prevLen:
-            f = open("data/AI_" + str(len(array[i-1])) + ".txt", "w+")
+            f = open("data/AI_" + str(prevLen) + ".txt", "w+")
             for word in array:
                 if word != "" and len(word) == prevLen:
                     f.write(word + "\n")
+                    
             f.close()
             finish = time.perf_counter()
-            print("\nNextNum: " + str(nextNum))
-            print("Prefix: " + str(prefix))
+            print("\nPrefix: " + str(prefix))
             print("Count: " + str(count))
             print(f"Dumpped length {prevLen} into 'data/AI_" + str(prevLen) + f".txt' in {round(finish-start, 2)} second(s)")
             prevLen += 1
+
+    f = open("data/AI_" + str(prevLen) + ".txt", "w+")
+    for word in array:
+        if word != "" and len(word) == prevLen:
+            f.write(word + "\n")
+    f.close()
+            
+    finish = time.perf_counter()
+    print("\nPrefix: " + str(prefix))
+    print("Count: " + str(count))
+    print(f"Dumpped length {prevLen} into 'data/AI_" + str(prevLen) + f".txt' in {round(finish-start, 2)} second(s)")
 
 
 def main():
